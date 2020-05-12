e82a3181787ec39105fa0e4a57b4ff27c96af8fe
iszkorla <iszkrola@users.noreply.github.com>
2020-05-12 10:42:20 -0500

    new updates to rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
@@ -6,11 +7,21 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 # Make a random walk, and plot the points.
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
+
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10,6))
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=10)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
-    time.sleep(7)
+    time.sleep(3)
 
     keep_running = input("Make another walk?  (y/n): ")
     if keep_running == 'n':
